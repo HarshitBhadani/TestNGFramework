23b752a2b263952bbcdc1398bd6bf3f7da0f3bc5
Harshit Bhadani <harshitbhadani62@gmail.com>
2022-05-03 18:11:47 +0530

    add commit

--- TestNGFramework/src/test/java/com/learnautomation/Utilities/Helper.java
+++ TestNGFramework/src/test/java/com/learnautomation/Utilities/Helper.java
@@ -12,7 +12,7 @@ public class Helper {
 	
 	public String captureScreenshot(WebDriver driver) {
 		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		String screenshotPath = System.getProperty("user.dir")+"/ScreenShots/Login.png";
+		String screenshotPath = System.getProperty("user.dir")+"./ScreenShots/Login.png";
 		try {
 			FileHandler.copy(src, new File(screenshotPath));
 		} catch (IOException e) {
